21f873c291411680a5472bf636c2a9c301fec985
Aditya Ningule <adiningule2@gmail.com>
2024-02-25 01:08:10 +0530

    poll system

--- agora-app-builder/expertsession/src/components/HostControlView.tsx
+++ agora-app-builder/expertsession/src/components/HostControlView.tsx
@@ -9,16 +9,14 @@
  information visit https://appbuilder.agora.io. 
 *********************************************
 */
-import React, {useContext, useRef, useState} from 'react';
-import {View, StyleSheet, Text, useWindowDimensions} from 'react-native';
+import React, {useRef, useState} from 'react';
+import {View, StyleSheet, useWindowDimensions} from 'react-native';
 import {useString} from '../utils/useString';
 import useRemoteMute, {MUTE_REMOTE_TYPE} from '../utils/useRemoteMute';
 import TertiaryButton from '../atoms/TertiaryButton';
 import Spacer from '../atoms/Spacer';
 import RemoteMutePopup from '../subComponents/RemoteMutePopup';
 import {calculatePosition} from '../utils/common';
-import TextInput from '../atoms/TextInput';
-import {PollContext} from './PollContext';
 import {
   I18nMuteType,
   peoplePanelMuteAllMicBtnText,
@@ -147,44 +145,21 @@ export const MuteAllVideoButton = (props: MuteAllVideoButtonProps) => {
 };
 
 const HostControlView = () => {
-  const [question, setQuestion] = useContext(PollContext);
   return (
     <View style={style.container}>
       {!$config.AUDIO_ROOM && <MuteAllVideoButton />}
       <Spacer horizontal size={16} />
       <MuteAllAudioButton />
-      <TextInput
-        value={question}
-        onChangeText={setQuestion}
-        placeholder="Poll Question"
-        style={style.textInput}
-      />
     </View>
   );
 };
 
 const style = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 20, // Add padding to the sides for better layout
-  },
-  buttonsContainer: {
     flexDirection: 'row',
-    alignItems: 'center',
-    paddingVertical: 20, // Add padding to the sides for better layout
+    justifyContent: 'center',
   },
-  textInput: {
-    width: '100%', // Take up full width
-    height: 40, // Adjust height as needed
-    backgroundColor: 'transparent', // Make text input background transparent
-    borderWidth: 1, // Add border for visibility
-    borderColor: 'white', // Border color white
-    borderRadius: 5, // Add some border radius for styling
-    paddingHorizontal: 10, // Add padding horizontally
-    color: 'white', // Text color white
-  },
+  btn: {},
 });
 
 export default HostControlView;
